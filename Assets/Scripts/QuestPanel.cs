984c676f5a066ceb6fe20d98617338daa953842d
Michael King <michael.ac.king@outlook.com>
2020-06-18 02:08:48 -0400

    Configure project for Android build

---
+++ Assets/Scripts/QuestPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestPanel : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI activeQuests;
+
+    public void UpdateQuestPanel()
+    {
+
+    }
+}
