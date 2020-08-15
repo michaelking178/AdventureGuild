8b1e0428acef50f693f2c3743a296c836047326c
Michael King <michael.ac.king@outlook.com>
2020-08-14 20:50:35 -0400

    Further building out the combat training minigame

---
+++ Assets/Scripts/Minigames/Training/Shield.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2[] quadrants = new Vector2[4];
+
+    private float repositionDelay = 2.0f;
+    private float startTime;
+    private float currentTime;
+    private int currentQuadrant;
+    private int newQuadrant;
+    private bool striking = false;
+    private TrainingManager trainingManager;
+    private TrainingSword trainingSword;
+
+    void Start()
+    {
+        trainingSword = FindObjectOfType<TrainingSword>();
+        trainingManager = FindObjectOfType<TrainingManager>();
+        startTime = Time.time;
+        currentTime = repositionDelay;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!trainingManager.GameOver)
+        {
+            if (currentTime < repositionDelay)
+            {
+                currentTime = Time.time - startTime;
+            }
+            else
+            {
+                ChangePosition();
+                startTime = Time.time;
+                currentTime = 0;
+            }
+        }
+    }
+
+    private void ChangePosition()
+    {
+        Vector2 newPos;
+        do
+        {
+            newQuadrant = Random.Range(0, quadrants.Length);
+            newPos = quadrants[newQuadrant];
+        }
+        while (newQuadrant == currentQuadrant);
+        transform.position = newPos;
+        currentQuadrant = newQuadrant;
+        repositionDelay *= 0.98f;
+    }
+
+    public void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        if (!trainingManager.GameOver && hit.collider != null)
+        {
+            trainingSword.Swing(hit.point);
+            StartCoroutine(StrikeShield());
+        }
+    }
+    
+    private IEnumerator StrikeShield()
+    {
+        if (!striking)
+        {
+            striking = true;
+            yield return new WaitForSeconds(0.2f);
+            FindObjectOfType<TrainingSword>().ClangSound();
+            trainingManager.AddPoints(Mathf.FloorToInt(trainingManager.TimeLimit / trainingManager.TimeRemaining) * 10);
+            currentTime = repositionDelay;
+            striking = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Minigames/Training/TrainingSword.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TrainingSword : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] swordClangs;
+
+    private AudioSource audioSource;
+    private Vector2 startPos;
+    private Vector2 movementPos;
+    private float distance;
+    private int speed = 25;
+    private float startTime = 0f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        startPos = transform.position;
+        movementPos = startPos;
+    }
+
+    private void FixedUpdate()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fraction = 0;
+        if (distCovered > 0)
+        {
+            fraction = distCovered / distance;
+        }
+        transform.position = Vector2.Lerp(startPos, movementPos, fraction);
+    }
+
+    public void Swing(Vector2 position)
+    {
+        if (!FindObjectOfType<TrainingManager>().GameOver)
+        {
+            StartCoroutine(SwingMovement(position));
+        }
+    }
+
+    private IEnumerator SwingMovement(Vector2 position)
+    {
+        startPos = transform.position;
+        movementPos = position;
+        startTime = Time.time;
+        distance = Vector2.Distance(startPos, movementPos);
+        yield return new WaitForSeconds(0.2f);
+
+        startPos = transform.position;
+        movementPos = Vector2.zero;
+        startTime = Time.time;
+        distance = Vector2.Distance(startPos, movementPos);
+        yield return new WaitForSeconds(0.2f);
+    }
+
+    public void ClangSound()
+    {
+        audioSource.clip = swordClangs[Random.Range(0, swordClangs.Length)];
+        audioSource.Play();
+    }
+}
