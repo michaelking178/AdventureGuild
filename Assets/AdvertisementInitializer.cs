036c9f58c652ceec91bac4e25535e1e2a36d426c
Michael King <michael.ac.king@outlook.com>
2021-03-13 23:12:19 -0500

    Add Advertisement Initializer, include App Store artwork

---
+++ Assets/AdvertisementInitializer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdvertisementInitializer : MonoBehaviour
+{
+    string gameId = "4049019";
+    bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+    }
+}
